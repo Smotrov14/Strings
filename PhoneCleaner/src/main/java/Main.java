85f34a97090a959761233b54373ae0854cdbeb4f
Smotrov14 <smotrov144@gmail.com>
2025-02-23 17:16:29 +0300

    Update Main.java

--- PhoneCleaner/src/main/java/Main.java
+++ PhoneCleaner/src/main/java/Main.java
@@ -10,8 +10,34 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+   input = input.replaceAll("[+\\s-()]", "");
+      char[] charArray =input.toCharArray();
+
+      if(charArray.length < 10 || charArray.length>11){
+          System.out.println("Неверный формат номера");
+          break;
+      }
+      if (charArray.length == 11 && charArray[0] == '8' ) {
+        charArray[0] = '7';
+      }
+      if (charArray.length == 11 && charArray[0] != '7' ){
+        System.out.println("Неверный формат номера");
+        break;
+      }
+      if (charArray.length == 10 && charArray[0] == '9' ) {
+        StringBuilder builder =new StringBuilder();
+        for(char i:charArray){
+          builder.append(i);
+        }
+        System.out.println("7"+ builder.toString());
+      }
+      if(charArray.length == 11 && charArray[1] == '9' ){
+        StringBuilder builder =new StringBuilder();
+        for(char i:charArray){
+          builder.append(i);
+        }
+        System.out.println(builder.toString());
+      }
+      }
     }
   }
-
-}
